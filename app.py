e780cde7a3ea26e3b97e6407c77f92d75ff50788
MatsValgaeren <dev@matsvalgaeren.com>
2025-07-28 21:58:01 +0200

    -Added DB

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'scretkey'
+app.config["SECRET_KEY"] = 'scretkey'
+app.config["UPLOAD_FOLDER"]
 
 class UploadFileForm(FlaskForm):
     file = FileField("file")
